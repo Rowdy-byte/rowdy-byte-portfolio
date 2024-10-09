565b352d91d94e1ff0c3bc5ef458de9b21dae20d
rowdy-byte <rowdy.verbeek@outlook.com>
2024-10-09 19:00:02 +0200

    refactored some code and added about and cards routes.

---
+++ src/lib/links.ts
@@ -0,0 +1,9 @@
+export const linksRowdyByte = [
+    { name: 'Portfolio', url: '/' },
+    { name: 'Documentation', url: '/docs' }
+];
+
+export const linksProjects = [
+    { name: 'Clash Royale Cards', url: '/cards' },
+    { name: 'Serialmapper', url: '/mapper' }
+];
