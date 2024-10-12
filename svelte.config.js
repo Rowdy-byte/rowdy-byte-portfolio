7eaaaba5926cc4985dea617c0e9dcad915a3d4f8
rowdy-byte <rowdy.verbeek@outlook.com>
2024-10-12 10:08:28 +0200

    added an if statement to check if there aren't running any instances of shiki. Before I didn't checked for it so there where 20 instances running. Oepsie...

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import { createHighlighter } from 'shiki';
 import { remarkHeaders } from './remark-headers.js'
 import rehypeSlug from 'rehype-slug';
 
-// import remarkToc from 'remark-toc';
+
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
@@ -13,31 +13,39 @@ const mdsvexOptions = {
 	remarkPlugins: [remarkHeaders],
 	rehypePlugins: [rehypeSlug],
 
+
+
 	highlight: {
+
 		highlighter: async (code, lang = 'text') => {
-			const highlighter = await createHighlighter({
-				themes: ['poimandres'],
-				langs: ['javascript', 'typescript', 'svelte', 'css', 'scss', 'html', 'bash']
-			});
+			let highlighter
+			if (!highlighter) {
+				highlighter = await createHighlighter({
+					themes: ['poimandres'],
+					langs: ['javascript', 'typescript', 'svelte', 'css', 'scss', 'html', 'bash']
+				})
+			}
+
 
 			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }));
 
 			return /* html */`
-			<div class="wrapper">
-				<div class="pre-header">
-						<button class="copy-button">
-							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clipboard"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"/><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/></svg>
-						</button>	
-					</div>
-							
-							${html}
-			</div>
-					
-					
-			`;
+				<div class="wrapper">
+					<div class="pre-header">
+							<button aria-label="copy-to-clipboard" class="copy-button">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clipboard"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"/><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/></svg>
+							</button>	
+						</div>
+								
+								${html}
+				</div>
+						
+						
+				`;
 		}
 	}
 }
+
 
 
 /** @type {import('@sveltejs/kit').Config} */
