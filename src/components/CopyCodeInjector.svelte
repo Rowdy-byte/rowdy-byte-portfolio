d15f47b5a70d9adecb9471e25296f66c3a4d4e9c
rowdy-byte <rowdy.verbeek@outlook.com>
2024-10-13 16:45:56 +0200

    added copytoclipboard functionality

--- src/components/CopyCodeInjector.svelte
+++ src/components/CopyCodeInjector.svelte
@@ -1,13 +1,44 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
+
+	import { setContext } from 'svelte';
+
 	let { children } = $props<{ children: Snippet }>();
 
+	let showToast = $state(false);
+
 	$effect(() => {
-		let pres: HTMLCollection = document.getElementsByTagName('pre');
-		console.log(pres);
+		setContext('showToast', showToast);
+		// console.log(showToast);
+	});
+
+	$effect(() => {
 		let buttons = document.querySelectorAll('#clipboardBtn');
-		console.log(buttons);
+
+		buttons.forEach((button) => {
+			button.addEventListener('click', () => {
+				const preElement = button.closest('.wrapper')?.querySelector('pre');
+
+				if (preElement) {
+					const codeElement = preElement as HTMLElement;
+					const code = codeElement.innerText;
+
+					navigator.clipboard.writeText(code).then(() => {
+						showToast = true;
+						console.log(showToast);
+
+						setTimeout(() => {
+							showToast = false;
+						}, 5000);
+					});
+				}
+			});
+		});
 	});
 </script>
+
+{#if showToast}
+	<p class="text-red-700 text-7xl absolute top-20">copied</p>
+{/if}
 
 {@render children()}
--- svelte.config.js
+++ svelte.config.js
@@ -38,7 +38,8 @@ const mdsvexOptions = {
 			return /*html */`
 			<div class="wrapper">
 							<div class="pre-header">
-								<button tabindex="-1" aria-label="copy-to-clipboard" class="copy-button">
+								
+								<button id="clipboardBtn" tabindex="-1" aria-label="copy-to-clipboard" class="copy-button">
 									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clipboard">
 										<rect width="8" height="4" x="8" y="2" rx="1" ry="1"/>
 										<path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/>
