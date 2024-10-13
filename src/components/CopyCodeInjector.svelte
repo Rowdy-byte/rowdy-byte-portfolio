c85017f3742b815794a12dc62dc937de1f9a6b0c
rowdy-byte <rowdy.verbeek@outlook.com>
2024-10-13 14:21:22 +0200

    added copycodeinjector comp

---
+++ src/components/CopyCodeInjector.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	let { children } = $props<{ children: Snippet }>();
+
+	$effect(() => {
+		let pres: HTMLCollection = document.getElementsByTagName('pre');
+		console.log(pres);
+		let buttons = document.querySelectorAll('#clipboardBtn');
+		console.log(buttons);
+	});
+</script>
+
+{@render children()}
