851f1b2b00dbb840896afdea0711d24df2466606
rowdy-byte <rowdy.verbeek@outlook.com>
2024-10-16 14:41:19 +0200

    update

--- src/routes/serialmapper/+page.svelte
+++ src/routes/serialmapper/+page.svelte
@@ -5,9 +5,9 @@
 
 <figure
 	use:animate={pageAnimation}
-	class="gap-2 flex flex-col absolute top-1/2 transform -translate-y-1/2 lg:left-1/2 lg:-translate-x-1/2"
+	class="gap-20 flex flex-col absolute top-1/2 transform -translate-y-1/2 lg:left-1/2 lg:-translate-x-1/2"
 >
-	<p class="text-slate-200 pb-2 font-cubano text-2xl font-bold text-center">
+	<p class="text-slate-200 font-cubano text-2xl font-bold text-center">
 		Serialmapper coming soon...
 	</p>
 	<img
@@ -15,7 +15,7 @@
 		src="/serialmapper_finished.jpg"
 		alt="serialmapper-design-img"
 	/>
-	<button class="bg-emerald-500 rounded m-2 py-2 px-8 text-slate-200 hover:scale-100"
+	<button class="bg-emerald-500 rounded m-8 py-2 px-8 text-slate-200 hover:scale-100"
 		><a href="/private/serialmapper-app">Go to App</a></button
 	>
 </figure>
