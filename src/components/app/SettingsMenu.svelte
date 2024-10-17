1b09736a46dd06a4f8d93956f8e9a08b9261394f
rowdy-byte <rowdy.verbeek@outlook.com>
2024-10-17 20:22:43 +0200

    .

--- src/components/app/SettingsMenu.svelte
+++ src/components/app/SettingsMenu.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <aside
-	class="text-center w-full sm:max-w-[450px] absolute top-1/4 right-2 ml-2 p-4 shadow-lg rounded-sm"
+	class="text-center w-full sm:max-w-[450px] absolute top-1/4 right-0 p-4 shadow-lg rounded-sm"
 >
 	{@render children()}
 </aside>
